7958d19b6d7a91ae8f4d387988b8f2d6326a75b5
Imagine0369 <mikephelps11@gmail.com>
2024-03-08 15:20:06 -0700

    added tailwind css dependencies

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  //purge content from files
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    // Add other file paths or globs if needed
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
